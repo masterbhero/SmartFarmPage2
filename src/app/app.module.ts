1ac7e14720124139e1a05ffc2461556fd4d28f83
pornpangrajeab <grajeab4@gmail.com>
2020-04-19 22:00:23 +0700

    หน้าเพิ่มอุปกรณ์-แก้ไข-จัดการอุปกรณ์

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatInputModule} from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import { PlotComponent } from './plot/plot.component';
+import { EditComponent } from './edit/edit.component';
+import { PlotMenuComponent } from './plot-menu/plot-menu.component';
+import { ManagedeviceComponent } from './managedevice/managedevice.component';
+import { AddeviceComponent } from './addevice/addevice.component';
 
 
 
 const Routes: Routes = [
-  { path: '',  redirectTo:'/login', pathMatch:'full' },
+  // { path: '',  redirectTo:'/login', pathMatch:'full' },
   { path: 'login',  component: LoginComponent },
+  { path: 'plot',  component: PlotComponent },
+  { path: 'edit',  component: EditComponent },
+  { path: 'managedevice',  component: ManagedeviceComponent },
   
 ];
 
@@ -22,7 +30,12 @@ const Routes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    PlotComponent,
+    EditComponent,
+    PlotMenuComponent,
+    ManagedeviceComponent,
+    AddeviceComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/plot-menu/plot-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-plot-menu',
+  templateUrl: './plot-menu.component.html',
+  styleUrls: ['./plot-menu.component.css']
+})
+export class PlotMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
